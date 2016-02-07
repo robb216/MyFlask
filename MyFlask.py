9df6c1eb8f456fca392ebae2d43e290b7e27f905
robb216 <robo@tryfirst.nl>
2016-02-07 14:01:40 +0100

    Initial commit

---
+++ MyFlask.py
@@ -0,0 +1,89 @@
+import sqlite3
+import os
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
+
+
+DATABASE = os.path.join(os.path.dirname(__file__), 'static', 'MyFlask.db')
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def check_user(username, password):
+    cur = g.db.execute('select count(*) from users where username = ? and password = ?', [username, password])
+    data = int(cur.fetchone()[0])
+    return data > 0
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select e.title, u.username, e.text from entries e, users u where u.id = e.owner order by u.id desc')
+    entries = [dict(title=row[0], owner=row[1], text=row[2]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text, owner) values (?, ?, (	select id from users where username = ?));',
+                 [request.form['title'], request.form['text'], session['username']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if not check_user(request.form['username'], request.form['password']):
+            error = 'Invalid login credentials'
+        else:
+            session['logged_in'] = True
+            session['username'] = request.form['username']
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    session.pop('username', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+if __name__ == '__main__':
+    init_db()
+    app.run()
---
+++ schema.sql
@@ -0,0 +1,17 @@
+pragma foreign_keys = ON;
+drop table if exists entries;
+drop table if exists users;
+create table IF NOT EXISTS users (
+  id integer primary key autoincrement,
+  username text not null,
+  password text not null,
+  UNIQUE(username)
+);
+create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  owner integer not null,
+  text text not null,
+  FOREIGN KEY(owner) REFERENCES users(id)
+);
+insert into users (username, password) values ('admin', 'default')
